0510fe5a675396e7db522d110527483a053e0edb
Tian Liu <liutian1113@gmail.com>
2023-02-19 04:10:24 +0000

    bugs fixing: delete old db/ folder, prod env setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -90,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  
 end
--- db/migrate/20220501043333_add_index_to_plan_models.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPlanModels < ActiveRecord::Migration[7.0]
-  def change
-    add_index :plan_models, :creator
-  end
-end
