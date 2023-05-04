b75317b7a0c73799b46ab90e83c813dab8298220
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2023-05-04 15:38:41 -0500

    add models

--- frontend/preview/docs/js/items.js
+++ frontend/preview/docs/js/items.js
@@ -1,184 +1,238 @@
 // add items to the "Add Items" tab
 
-$(document).ready(function() {
-  var items = [
-   {
-      "name" : "Closed Door",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
-      "model" : "models/js/closed-door28x80_baked.js",
-      "type" : "7"
-    }, 
-    {
-      "name" : "Open Door",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
-      "model" : "models/js/open_door.js",
-      "type" : "7"
-    }, 
-    {
-      "name" : "Window",
-      "image" : "models/thumbnails/thumbnail_window.png",
-      "model" : "models/js/whitewindow.js",
-      "type" : "3"
-    }, 
-    {
-      "name" : "Chair",
-      "image" : "models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
-      "model" : "models/js/gus-churchchair-whiteoak.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Red Chair",
-      "image" : "models/thumbnails/thumbnail_tn-orange.png",
-      "model" : "models/js/ik-ekero-orange_baked.js",
-      "type" : "1"
-    },
-    {
-      "name" : "Blue Chair",
-      "image" : "models/thumbnails/thumbnail_ekero-blue3.png",
-      "model" : "models/js/ik-ekero-blue_baked.js",
-      "type" : "1"
-    },
-    {
-      "name" : "Dresser - Dark Wood",
-      "image" : "models/thumbnails/thumbnail_matera_dresser_5.png",
-      "model" : "models/js/DWR_MATERA_DRESSER2.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Dresser - White",
-      "image" : "models/thumbnails/thumbnail_img25o.jpg",
-      "model" : "models/js/we-narrow6white_baked.js",
-      "type" : "1"
-    },  
-    {
-      "name" : "Bedside table - Shale",
-      "image" : "models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
-      "model" : "models/js/bd-shalebedside-smoke_baked.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Bedside table - White",
-      "image" : "models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
-      "model" : "models/js/cb-archnight-white_baked.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Wardrobe - White",
-      "image" : "models/thumbnails/thumbnail_TN-ikea-kvikine.png",
-      "model" : "models/js/ik-kivine_baked.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Full Bed",
-      "image" : "models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
-      "model" : "models/js/ik_nordli_full.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Bookshelf",
-      "image" : "models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
-      "model" : "models/js/cb-kendallbookcasewalnut_baked.js",
-      "type" : "1"
-    }, 
+$(document).ready(function () {
+    var items = [
         {
-      "name" : "Media Console - White",
-      "image" : "models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
-      "model" : "models/js/cb-clapboard_baked.js",
-      "type" : "1"
-    }, 
+            "name": "Closed Door",
+            "image": "models/thumbnails/door_closed.png",
+            "model": "models/js/door_closed.js",
+            "type": "7"
+        },
         {
-      "name" : "Media Console - Black",
-      "image" : "models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
-      "model" : "models/js/cb-moore_baked.js",
-      "type" : "1"
-    }, 
-       {
-      "name" : "Sectional - Olive",
-      "image" : "models/thumbnails/thumbnail_img21o.jpg",
-      "model" : "models/js/we-crosby2piece-greenbaked.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Sofa - Grey",
-      "image" : "models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
-      "model" : "models/js/cb-rochelle-gray_baked.js",
-      "type" : "1"
-    }, 
+            "name": "Open Door",
+            "image": "models/thumbnails/door_open.png",
+            "model": "models/js/door_open.js",
+            "type": "7"
+        },
         {
-      "name" : "Wooden Trunk",
-      "image" : "models/thumbnails/thumbnail_teca-storage-trunk.jpg",
-      "model" : "models/js/cb-tecs_baked.js",
-      "type" : "1"
-    }, 
+            "name": "Window",
+            "image": "models/thumbnails/window.png",
+            "model": "models/js/window.js",
+            "type": "3"
+        },
         {
-      "name" : "Floor Lamp",
-      "image" : "models/thumbnails/thumbnail_ore-white.png",
-      "model" : "models/js/ore-3legged-white_baked.js",
-      "type" : "1"
-    },
-    {
-      "name" : "Coffee Table - Wood",
-      "image" : "models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
-      "model" : "models/js/ik-stockholmcoffee-brown.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Side Table",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
-      "model" : "models/js/GUSossingtonendtable.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Dining Table",
-      "image" : "models/thumbnails/thumbnail_scholar-dining-table.jpg",
-      "model" : "models/js/cb-scholartable_baked.js",
-      "type" : "1"
-    }, 
-    {
-      "name" : "Dining table",
-      "image" : "models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
-      "model" : "models/js/BlakeAvenuejoshuatreecheftable.js",
-      "type" : "1"
-    },
-    {
-      "name" : "Blue Rug",
-      "image" : "models/thumbnails/thumbnail_cb-blue-block60x96.png",
-      "model" : "models/js/cb-blue-block-60x96.js",
-      "type" : "8"
-    },
-    {
-      "name" : "NYC Poster",
-      "image" : "models/thumbnails/thumbnail_nyc2.jpg",
-      "model" : "models/js/nyc-poster2.js",
-      "type" : "2"
+            "name": "Chair",
+            "image": "models/thumbnails/chair.jpg",
+            "model": "models/js/chair.js",
+            "type": "1"
+        },
+        {
+            "name": "Red Chair",
+            "image": "models/thumbnails/chair_red.png",
+            "model": "models/js/chair_red.js",
+            "type": "1"
+        },
+        {
+            "name": "Blue Chair",
+            "image": "models/thumbnails/chair_blue.png",
+            "model": "models/js/chair_blue.js",
+            "type": "1"
+        },
+        {
+            "name": "Sofa",
+            "image": "models/thumbnails/sofa.jpg",
+            "model": "models/js/sofa.js",
+            "type": "1"
+        },
+        {
+            "name": "Media Console",
+            "image": "models/thumbnails/media.jpg",
+            "model": "models/js/media.js",
+            "type": "1"
+        },
+        {
+            "name": "Floor Lamp",
+            "image": "models/thumbnails/lamp.png",
+            "model": "models/js/lamp.js",
+            "type": "1"
+        },
+        {
+            "name": "Shelf",
+            "image": "models/thumbnails/shelf.png",
+            "model": "models/js/shelf.js",
+            "type": "1"
+        },
+        {
+            "name": "Bookshelf",
+            "image": "models/thumbnails/bookcase.jpg",
+            "model": "models/js/bookcase.js",
+            "type": "1"
+        },
+        /*
+        {
+            "name": "Storage",
+            "image": "models/thumbnails/storage.jpg",
+            "model": "models/js/storage.js",
+            "type": "1"
+        },
+        */
+        {
+            "name": "Wooden Trunk",
+            "image": "models/thumbnails/trunk.jpg",
+            "model": "models/js/trunk.js",
+            "type": "1"
+        },
+        {
+            "name": "Coffee Table",
+            "image": "models/thumbnails/table_coffee.jpg",
+            "model": "models/js/table_coffee.js",
+            "type": "1"
+        },
+        {
+            "name": "Meeting Table",
+            "image": "models/thumbnails/table_rec.png",
+            "model": "models/js/table_rec.js",
+            "type": "1"
+        },
+        {
+            "name": "Round Table",
+            "image": "models/thumbnails/table_rnd.jpg",
+            "model": "models/js/table_rnd.js",
+            "type": "1"
+        },
+        {
+            "name": "Side Table",
+            "image": "models/thumbnails/table_side.png",
+            "model": "models/js/table_side.js",
+            "type": "1"
+        },
+        {
+            "name": "Man",
+            "image": "models/thumbnails/man.png",
+            "model": "models/js/man.js",
+            "type": "1"
+        },
+        {
+            "name": "Woman",
+            "image": "models/thumbnails/woman.png",
+            "model": "models/js/woman.js",
+            "type": "1"
+        },
+        {
+            "name": "Podium",
+            "image": "models/thumbnails/podium.png",
+            "model": "models/js/podium.js",
+            "type": "1"
+        },
+        {
+            "name": "Array of Chairs (8)",
+            "image": "models/thumbnails/arr8.png",
+            "model": "models/js/arr8.js",
+            "type": "1"
+        },
+        {
+            "name": "Array of Chairs (14)",
+            "image": "models/thumbnails/arr14.png",
+            "model": "models/js/arr14.js",
+            "type": "1"
+        },
+        {
+            "name": "Rectangle Table Set (2)",
+            "image": "models/thumbnails/rec2.png",
+            "model": "models/js/rec2.js",
+            "type": "1"
+        },
+        {
+            "name": "Rectangle Table Set (4)",
+            "image": "models/thumbnails/rec4.png",
+            "model": "models/js/rec4.js",
+            "type": "1"
+        },
+        {
+            "name": "Rectangle Table Set (8)",
+            "image": "models/thumbnails/rec8.png",
+            "model": "models/js/rec8.js",
+            "type": "1"
+        },
+        {
+            "name": "Round Table Set (2)",
+            "image": "models/thumbnails/rnd2.png",
+            "model": "models/js/rnd2.js",
+            "type": "1"
+        },
+        {
+            "name": "Round Table Set (4)",
+            "image": "models/thumbnails/rnd4.png",
+            "model": "models/js/rnd4.js",
+            "type": "1"
+        },
+        {
+            "name": "Round Table Set (8)",
+            "image": "models/thumbnails/rnd8.png",
+            "model": "models/js/rnd8.js",
+            "type": "1"
+        },
+        {
+            "name": "Square Table Set (2)",
+            "image": "models/thumbnails/sq2.png",
+            "model": "models/js/sq2.js",
+            "type": "1"
+        },
+        {
+            "name": "Square Table Set (4)",
+            "image": "models/thumbnails/sq4.png",
+            "model": "models/js/sq4.js",
+            "type": "1"
+        },
+        {
+            "name": "Square Table Set (8)",
+            "image": "models/thumbnails/sq8.png",
+            "model": "models/js/sq8.js",
+            "type": "1"
+        },
+        {
+            "name": "Camera",
+            "image": "models/thumbnails/camera.png",
+            "model": "models/js/camera.js",
+            "type": "1"
+        },
+        {
+            "name": "Poster",
+            "image": "models/thumbnails/poster.jpg",
+            "model": "models/js/poster.js",
+            "type": "2"
+        },
+        /*
+        {
+            "name": "Blue Rug",
+            "image": "models/thumbnails/rug_blue.png",
+            "model": "models/js/rug_blue.js",
+            "type": "8"
+        },
+        */
+        {
+            "name": "Color Rug",
+            "image": "models/thumbnails/rug.png",
+            "model": "models/js/rug.js",
+            "type": "8"
+        }
+    ]
+
+    var itemsDiv = $("#items-wrapper")
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        var html = '<div class="col-sm-4">' +
+            '<a class="thumbnail add-item" model-name="' +
+            item.name +
+            '" model-url="' +
+            item.model +
+            '" model-type="' +
+            item.type +
+            '"><img src="' +
+            item.image +
+            '" alt="Add Item"> ' +
+            item.name +
+            '</a></div>';
+        itemsDiv.append(html);
     }
-   /*     
-   {
-      "name" : "",
-      "image" : "",
-      "model" : "",
-      "type" : "1"
-    }, 
-    */
-  ]
-
-
-
-  var itemsDiv = $("#items-wrapper")
-  for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    var html = '<div class="col-sm-4">' +
-                '<a class="thumbnail add-item" model-name="' + 
-                item.name + 
-                '" model-url="' +
-                item.model +
-                '" model-type="' +
-                item.type + 
-                '"><img src="' +
-                item.image + 
-                '" alt="Add Item"> '+
-                item.name +
-                '</a></div>';
-    itemsDiv.append(html);
-  }
 });
